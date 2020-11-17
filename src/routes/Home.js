c5353b4347656e1f12052a1525c4f1f23ab9c8e5
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-17 16:26:47 +0900

    home loading 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,5 +12,10 @@ const GET_MOVIES = gql`
 export default () => {
   const {loading, error, data} = useQuery(GET_MOVIES);
   console.log(loading, error, data);
-  return <h1>Home</h1>;
+  if (loading) {
+    return 'loading...';
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
 };
