dfd4461a1d14f6214edcc21b0aab3415005481ce
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-17 14:09:46 +0900

    detail, home page 생성 및 라우터 연결

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
+import {HashRouter as Router, Route} from 'react-router-dom';
+
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/:id" component={Detail}></Route>
+    </Router>
+  );
 }
 
 export default App;
