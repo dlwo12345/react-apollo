fd346efd29790956e6578113a2178bf9a3d9c145
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-17 15:49:42 +0900

    apollo 요청 url 변경

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,7 @@ import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 // ref: https://www.apollographql.com/docs/react/get-started/
 const client = new ApolloClient({
-  uri: 'https://movieql.now.sh',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
