cc8fe53c6bd34f774fab1b3b93a9c5e0247e8457
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-17 15:50:01 +0900

    home.js gql 요청

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,16 @@
 import React from 'react';
-export default () => <h1>Home</h1>;
+import {gql, useQuery} from '@apollo/client';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+export default () => {
+  const {loading, error, data} = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  return <h1>Home</h1>;
+};
