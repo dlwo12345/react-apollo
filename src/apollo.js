b38bed206dc0491f82912681d0c3062dd34a22db
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-18 12:14:36 +0900

    local state 제어 예제 추가

--- src/apollo.js
+++ src/apollo.js
@@ -4,6 +4,22 @@ import {ApolloClient, InMemoryCache} from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      likeMovie: (_, {id}, {cache}) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: true,
+            medium_cover_image: 'lalalalal',
+          },
+        });
+      },
+    },
+  },
 });
 
 export default client;
